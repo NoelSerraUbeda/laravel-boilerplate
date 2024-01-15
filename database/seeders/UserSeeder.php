667d38ab5d811357ffba68f0e24362e270503888
NoelSerraUbeda <neweonj@gmail.com>
2024-01-15 12:01:16 +0100

    seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                "name" => "Noel",
+                "emai" => "NoelSerra@gmail.com",
+                "password" =>"$2y$12$7UdOouuoovBQEqEZDfFziOSxKSQZxI7lUyAobGLXelfZoYtAVwbCi",
+            ],
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
+
+
